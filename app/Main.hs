46db74c39252a71773cce58a668f534f9a17acfe
Pontus Hjortskog <p.hjortskog@gmail.com>
2021-12-03 11:19:43 +0100

    Add day 3 to main program

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Day1
 import Day2
+import Day3
 
 main :: IO ()
 main = do
@@ -10,3 +11,4 @@ main = do
     case read number of
         1 -> Day1.solve
         2 -> Day2.solve
+        3 -> Day3.solve
